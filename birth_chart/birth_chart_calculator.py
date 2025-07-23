71ac08f69cc9471f25b2ddc287e080d23c90959a
ProtocolChecker <florian.bargues@fountainhead.digital>
2025-07-23 19:44:43 -0300

    update import birth chart

--- birth_chart/birth_chart_calculator.py
+++ birth_chart/birth_chart_calculator.py
@@ -17,7 +17,7 @@
 
 # birth_chart_calculator.py
 
-from .birth_chart.astrology_calculator import AstrologyCalculator
+from .astrology_calculator import AstrologyCalculator
 
 def calculate_birth_chart(
     birth_date,
